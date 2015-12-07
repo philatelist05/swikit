8f2d65f9f70d81da293e94feb216594ee2fa3abb
Stefan Gamerith <steff7@gmx.com>
2015-12-07 15:33:49 +0100

    Added tests for stack implementation

---
+++ test/alltests.fs
@@ -0,0 +1,12 @@
+\ Run all tests
+
+\ For testing John Hayes "ttester.fth" is used.
+\ It may be necessary to adjust the file paths
+\ accordingly.
+
+CR .( Running all tests ) CR
+
+  S" ttester.fth" INCLUDED
+  S" stacktest.fs" INCLUDED
+
+CR CR .( tests completed ) CR CR
---
+++ test/stacktest.fs
@@ -0,0 +1,53 @@
+S" ../stack.fs" INCLUDED
+
+\ ------------------------------------------------------------------------
+TESTING BASIC ASSUMPTIONS
+
+T{ -> }T
+
+\ ------------------------------------------------------------------------
+TESTING EMPTY STACK
+
+0 INIT-STACK VALUE s0
+: catchpop ['] POP CATCH DROP ;
+: catchpush ['] PUSH CATCH DROP ;
+
+T{ s0 SIZE -> 0 }T
+T{ s0 catchpop -> s0 }T
+T{ 9 s0 catchpush -> 9 s0 }T
+
+\ ------------------------------------------------------------------------
+TESTING STACK WITH ONE ELEMENT
+
+1 INIT-STACK VALUE s1
+
+T{ 9 s1 PUSH -> }T
+T{ s1 SIZE -> 1 }T
+
+T{ s1 POP -> 9 }T
+T{ s1 SIZE -> 0 }T
+
+\ ------------------------------------------------------------------------
+TESTING LIFO ORDER
+
+3 INIT-STACK VALUE s3
+
+T{ 10 s3 PUSH -> }T
+T{ 11 s3 PUSH -> }T
+T{ 12 s3 PUSH -> }T
+
+T{ s3 SIZE -> 3 }T
+T{ s3 POP -> 12 }T
+T{ s3 POP -> 11 }T
+T{ s3 POP -> 10 }T
+
+\ ------------------------------------------------------------------------
+TESTING STORING ADDRESSES
+
+1 INIT-STACK VALUE s1-addr
+
+HERE CONSTANT addr
+
+T{ addr s1-addr PUSH -> }T
+T{ s1-addr SIZE -> 1 }T
+T{ s1-addr POP -> addr }T
