02042157d4afa436d428bc0de41a35a8f77a5ec8
Stefan Gamerith <steff7@gmx.com>
2015-12-07 11:57:26 +0100

    Encoded stack size into memory

--- stack.fs
+++ stack.fs
@@ -1,17 +1,18 @@
-10 CONSTANT stack-size
-
 
 : OVERFLOW? ( a-addr -- )
-  @ stack-size = THROW
+  DUP CELL+ @ \ retrieve capacity
+  SWAP @ = THROW \ capacity == offset
 ;
 
 : UNDERFLOW? ( a-addr -- )
   @ 0 = THROW
 ;
 
-: INIT-STACK ( -- a-addr )
-	HERE stack-size 1+ CELLS ALLOT >R
+: INIT-STACK ( n -- a-addr )
+\ initializes the stack with capacity of n
+	DUP HERE SWAP 2 + CELLS ALLOT >R
   0 R@ ! \ number of current stack elements
+  R@ CELL+ ! \ store the stack capacity
   R>
 ;
 
@@ -19,15 +20,20 @@
 	\ takes a value from the data stack
 	\ and pushes it onto the stack
   DUP OVERFLOW?
-  1 OVER +! \ #stack elements ++
-  DUP @ CELLS + ! \ store n
+  1 OVER +! \ offset++
+  DUP @ 1+ CELLS + ! \ store n
 ;
 
 : POP ( a-addr -- n )
   \ takes a value from the stack
   \ and pushes it onto the data stack
   DUP UNDERFLOW?
-  DUP DUP @ CELLS + @ >R \ retrieve element
-  DUP @ 1- SWAP ! \ #stack elements --
+  DUP DUP @ 1+ CELLS + @ >R \ retrieve element
+  DUP @ 1- SWAP ! \ offset--
   R>
+;
+
+: SIZE ( a-addr -- n )
+  \ pushes the number of elements
+  @
 ;
