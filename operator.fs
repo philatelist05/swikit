5c04629d8b5ca2282bf837a07435704a4b17d7e8
Stefan Gamerith <e0925081@student.tuwien.ac.at>
2015-12-13 02:01:09 +0100

    Added rules for infix conversion (Shunting-yard algorithm)

--- operator.fs
+++ operator.fs
@@ -1,24 +1,35 @@
+INCLUDE stack.fs
+
+\ associativity
+0 CONSTANT left
+1 CONSTANT right
+2 CONSTANT none
+
+\ offsets for decision table
+1 CONSTANT condition
+0 CONSTANT action
+
+\ Stacks
+15 CONSTANT stack-size
+CREATE operatorstack stack-size INIT-STACK ,
+CREATE operandstack stack-size INIT-STACK ,
+
+
 \ Operator implementations
-: power
-;
+DEFER power
 
-: mul
-;
+DEFER mul
 
-: div
-;
+DEFER div
 
-: plus
-;
+DEFER plus
 
-: minus
-;
+DEFER minus
 
-: p(
-;
+DEFER p(
 
-: p)
-;
+DEFER p)
+
 
 : OPERATOR ( n1 n2 n3 n4 --  )
 	CREATE , , , ,
@@ -26,41 +37,56 @@
 	SWAP CELLS + @
 ;
 
-\ associativity
-0 CONSTANT left
-1 CONSTANT right
-2 CONSTANT none
-
-\ Syntactic sugar
-: prec ( xt -- n )
+\ Some helper words
+: prec ( op-xt -- n )
 	0 SWAP EXECUTE
 ;
 
-: #params ( xt -- n )
+: #params ( op-xt -- n )
 	2 SWAP EXECUTE
 ;
 
-: exec ( xt -- ... )
+: exec ( op-xt -- )
 	3 SWAP EXECUTE
+	EXECUTE
 ;
 
-: is-left? ( xt -- ? )
+: is-left? ( op-xt -- ? )
 	1 SWAP EXECUTE
 	left =
 ;
 
-: is-right? ( xt -- ? )
+: is-right? ( op-xt -- ? )
 	1 SWAP EXECUTE
 	right =
 ;
 
 
+
+\ Definition of Rules
+\ see : http://csis.pace.edu/~wolf/CS122/infix-postfix.htm
+: RULE ( xt1 xt2 -- )
+	CREATE , ,
+	DOES> ( n addr -- )
+	SWAP CELLS + @
+;
+
+
+\ Condition ( stack op-xt -- ? )    Action ( stack op-xt -- )
+:NONAME SWAP DROP #params 0 = ;     :NONAME exec DROP ;          RULE rule1 \ no params -> directly evaluate
+:NONAME prec SWAP POP prec > ;      :NONAME SWAP PUSH ;          RULE rule2 \ prec of incoming symbol > prec top of stack -> push
+:NONAME prec SWAP POP prec < ;      :NONAME SWAP POP exec PUSH ; RULE rule3 \ prec of incoming symbol < prec top of stack -> pop top and push incoming
+:NONAME SWAP DROP is-left? ;        :NONAME SWAP POP exec PUSH ; RULE rule4 \ equal prec and left-assoc -> pop top and push incoming
+:NONAME SWAP DROP is-right? ;       :NONAME SWAP PUSH ;          RULE rule5 \ equal prec and right-assoc -> push
+
+
 \ func  #params   assoc  precedence          name
-' power  2        right      4       OPERATOR ^
-' mul    2        left       3       OPERATOR *
-' div    2        left       3       OPERATOR /
-' plus   2        left       2       OPERATOR +
-' minus  2        left       2       OPERATOR -
-' p(     0        left       0       OPERATOR (
-' p)     0        left       0       OPERATOR )
+' power   2        right      4       OPERATOR ^
+' mul     2        left       3       OPERATOR *
+' div     2        left       3       OPERATOR /
+' plus    2        left       2       OPERATOR +
+' minus   2        left       2       OPERATOR -
+' p(      0        left       0       OPERATOR (
+' p)      0        left       0       OPERATOR )
+
 
--- test/operatortest.fs
+++ test/operatortest.fs
@@ -8,59 +8,99 @@ TESTING BASIC ASSUMPTIONS
 T{ -> }T
 
 \ ------------------------------------------------------------------------
-TESTING '^'
+TESTING BASIC ARITHMETIC OPERATORS
+
+\ overwrite operator implementations
+
+:NONAME '^' ; IS power
+:NONAME '*' ; IS mul
+:NONAME '/' ; IS div
+:NONAME '+' ; IS plus
+:NONAME '-' ; IS minus
+:NONAME '(' ; IS p(
+:NONAME ')' ; IS p)
+
 
 T{ ' ^ #params  -> 2 }T
 T{ ' ^ is-left?  -> FALSE }T
 T{ ' ^ is-right?  -> TRUE }T
 T{ ' ^ prec  -> 4 }T
-
-\ ------------------------------------------------------------------------
-TESTING '*'
+T{ ' ^ exec  ->  '^' }T
 
 T{ ' * #params  -> 2 }T
 T{ ' * is-left?  -> TRUE }T
 T{ ' * is-right?  -> FALSE }T
 T{ ' * prec  -> 3 }T
-
-\ ------------------------------------------------------------------------
-TESTING '/'
+T{ ' * exec  ->  '*' }T
 
 T{ ' / #params  -> 2 }T
 T{ ' / is-left?  -> TRUE }T
 T{ ' / is-right?  -> FALSE }T
 T{ ' / prec  -> 3 }T
-
-\ ------------------------------------------------------------------------
-TESTING '+'
+T{ ' / exec  ->  '/' }T
 
 T{ ' + #params  -> 2 }T
 T{ ' + is-left?  -> TRUE }T
 T{ ' + is-right?  -> FALSE }T
 T{ ' + prec  -> 2 }T
-
-\ ------------------------------------------------------------------------
-TESTING '-'
+T{ ' + exec  ->  '+' }T
 
 T{ ' - #params  -> 2 }T
 T{ ' - is-left?  -> TRUE }T
 T{ ' - is-right?  -> FALSE }T
 T{ ' - prec  -> 2 }T
-
-\ ------------------------------------------------------------------------
-TESTING '('
+T{ ' - exec  ->  '-' }T
 
 T{ ' ( #params  -> 0 }T
 T{ ' ( is-left?  -> TRUE }T
 T{ ' ( is-right?  -> FALSE }T
 T{ ' ( prec  -> 0 }T
-
-\ ------------------------------------------------------------------------
-TESTING ')'
+T{ ' ( exec  ->  '(' }T
 
 T{ ' ) #params  -> 0 }T
 T{ ' ) is-left?  -> TRUE }T
 T{ ' ) is-right?  -> FALSE }T
 T{ ' ) prec  -> 0 }T
+T{ ' ) exec  ->  ')' }T
 
 \ ------------------------------------------------------------------------
+TESTING RULES
+
+: +stack
+	3 INIT-STACK
+	DUP
+	['] + SWAP PUSH
+;
+
+
+T{ +stack ' ( condition rule1 EXECUTE -> TRUE }T
+T{ +stack ' ) condition rule1 EXECUTE -> TRUE }T
+T{ +stack ' + condition rule1 EXECUTE -> FALSE }T
+T{ +stack ' - condition rule1 EXECUTE -> FALSE }T
+T{ +stack ' * condition rule1 EXECUTE -> FALSE }T
+T{ +stack ' / condition rule1 EXECUTE -> FALSE }T
+
+T{ +stack ' + condition rule2 EXECUTE -> FALSE }T
+T{ +stack ' - condition rule2 EXECUTE -> FALSE }T
+T{ +stack ' * condition rule2 EXECUTE -> TRUE }T
+T{ +stack ' / condition rule2 EXECUTE -> TRUE }T
+T{ +stack ' ^ condition rule2 EXECUTE -> TRUE }T
+
+T{ +stack ' + condition rule3 EXECUTE -> FALSE }T
+T{ +stack ' - condition rule3 EXECUTE -> FALSE }T
+T{ +stack ' * condition rule3 EXECUTE -> FALSE }T
+T{ +stack ' / condition rule3 EXECUTE -> FALSE }T
+T{ +stack ' ^ condition rule3 EXECUTE -> FALSE }T
+
+T{ +stack ' + condition rule4 EXECUTE -> TRUE }T
+T{ +stack ' - condition rule4 EXECUTE -> TRUE }T
+T{ +stack ' * condition rule4 EXECUTE -> TRUE }T
+T{ +stack ' / condition rule4 EXECUTE -> TRUE }T
+T{ +stack ' ^ condition rule4 EXECUTE -> FALSE }T
+
+T{ +stack ' + condition rule5 EXECUTE -> FALSE }T
+T{ +stack ' - condition rule5 EXECUTE -> FALSE }T
+T{ +stack ' * condition rule5 EXECUTE -> FALSE }T
+T{ +stack ' / condition rule5 EXECUTE -> FALSE }T
+T{ +stack ' ^ condition rule5 EXECUTE -> TRUE }T
+
